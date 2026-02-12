a89a0177a6e8a35ca7b54c468e546bf929828f6b
Rich Tillman <effinrich@pm.me>
2026-02-12 10:37:27 -0500

    feat: integrate @vercel/speed-insights into the main application

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '@forgekit-landing/theme'
-import { App } from './app'
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '@forgekit-landing/theme';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
+import { App } from './app';
 
 const root = document.getElementById('root')
 
 if (root) {
   createRoot(root).render(
     <StrictMode>
+      <SpeedInsights />
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
