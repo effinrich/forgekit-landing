9bddc2503907a432d6ac009c1e20eb212ddac57e
Rich Tillman <effinrich@pm.me>
2026-08-06 02:27:34 -0400

    chore: [nx migration] rename-rollup-options-to-rolldown-options

--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       fileName: 'index',
       formats: ['es'],
     },
-    rollupOptions: {
+    rolldownOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
